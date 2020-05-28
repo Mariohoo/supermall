bddf28cec15a7f6cb646b61f8418d093aa8f4c81
Mariohoo <324095048@qq.com>
2020-05-28 20:34:27 +0800

    详细页

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,25 +1,70 @@
 <template>
-    <detail-nav-bar></detail-nav-bar>
+    <div id="detail">
+         <detail-nav-bar></detail-nav-bar>
+        <detail-swiper :topImages="topImages"></detail-swiper>
+        <detail-base-info :goods="goods"></detail-base-info>
+        <detail-shop-info :shop="shop"></detail-shop-info>
+        <detail-goods-info :detailInfo="detailInfo"></detail-goods-info>
+        <detail-param-info :paramInfo="paramInfo"></detail-param-info>
+    </div>
 </template>
 
 <script>
+import DetailParamInfo from './childcomponents/DetailParamInfo'
+import DetailGoodsInfo from './childcomponents/DetailGoodsInfo'
+import DetailShopInfo from './childcomponents/DetailShopInfo'
 import DetailNavBar from './childcomponents/DetailNavBar'
+import DetailSwiper from './childcomponents/DetailSwiper'
+import DetailBaseInfo from './childcomponents/DetailBaseInfo'
+import {getDetail,Goods,Shop,GoodsParam} from '../../network/detail'
+import Scroll from '../../components/common/scroll/Scroll'
 export default {
     name:'Detail',
     components:{
-        DetailNavBar
+        DetailNavBar,
+        DetailSwiper,
+        DetailBaseInfo,
+        DetailShopInfo,
+        DetailGoodsInfo,
+        DetailParamInfo
     },
     data(){
         return {
-            iid: null
+            iid: null,
+            topImages:[],
+            goods:{},
+            shop:{},
+            detailInfo:{},
+            paramInfo:{}
         }
     },
     created(){
         this.iid = this.$route.params.iid
+
+        getDetail(this.iid).then(res=>{
+            console.log(res);
+            const data = res.result
+            this.topImages =  data.itemInfo.topImages
+            
+            this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+            console.log(this.goods);
+            
+            this.shop = new Shop(data.shopInfo)
+            
+            this.detailInfo = data.detailInfo;
+
+            this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
+        })
+
+
     }
 }
 </script>
 
 <style scoped>
-
+#detail{
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+}
 </style>
---
+++ src/views/detail/childcomponents/DetailSwiper.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="detail-swiper">
+        <swiper class="swiper1">
+            <swiper-item v-for="(item,index) in topImages" :key="index" class="swiper-item">
+                <img :src="item" alt="">
+            </swiper-item>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import Swiper from '../../../components/common/swiper/Swiper'
+import SwiperItem from '../../../components/common/swiper/SwiperItem'
+
+export default {
+    name:'DetailSwiper',
+    components:{
+        Swiper,
+        SwiperItem
+    },
+    props:{
+        topImages:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.swiper1{
+    height: 300px;
+    overflow: hidden;
+}
+</style>
